ddbc128ad33d937daf17003372c8b003fae94420
ercane <ercane@PBTH-1>
2019-07-27 12:56:45 +0300

    Add project files.

---
+++ ConfirmDialogExample/Controllers/StudentController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfirmDialogExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfirmDialogExample.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        //static student list with samples
+        static IList<Student> StudentList = new List<Student>() {
+            new Student(){Id=1, Firstname="John", Lastname="Galt"},
+            new Student(){Id=2, Firstname="Jane", Lastname="Malt"},
+            new Student(){Id=3, Firstname="Mary", Lastname="Salt"}
+        };
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(StudentList);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This action creates a Student and insert into StudentList.
+        /// It is called by the ajax requests which can evaluate return values.
+        /// 
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns>Json object which keeps 3 different values.
+        ///     "state" (mandatory) 0 is sent when success occurs, other values sent in failures.
+        ///     "msg" (mandatory) Description of the result. Message to the user.
+        ///     "redirectUrlInSuccess" (optional) This is used to redirect the user to another page after 
+        ///     successful operations. If null or empty, user sees the same page after closing messages.
+        /// </returns>
+        // POST: Student/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student)
+        {
+            if (student == null)
+            {
+                return Json(
+                    new
+                    {
+                        state = 1, //1:random number for fail code
+                        msg = "Student Model is NULL"
+                    }
+                );
+            }
+
+            if (StudentList.Any(a => a.Firstname == student.Firstname && a.Lastname == student.Lastname))
+            {
+                return Json(
+                    new
+                    {
+                        state = 2, //2:random number for fail code
+                        msg = "Student already EXISTS!"
+                    }
+                );
+            }
+
+            try
+            {
+                int maxUsedId = StudentList.Max(a => a.Id);
+                student.Id = ++maxUsedId;
+                StudentList.Add(student);
+
+                return Json(
+                    new
+                    {
+                        state = 0, //0 for success
+                        msg = "Student has been CREATED successfully.",
+                        redirectUrlInSuccess = "Index" // Goes to /Student/Index page. If we want to be redirected to page under another controller, we could use /Home/Index.
+                    }
+                );
+            }
+            catch (Exception e)
+            {
+                return Json(
+                    new
+                    {
+                        state = 99, //99:random number for fail code
+                        msg = "Exception occurred while user in creation!" + e.Message
+                    }
+                );
+            }
+        }
+
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ConfirmDialogExample/wwwroot/js/ajaxDialog.js
@@ -0,0 +1,81 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file contains ajax request functions to be used when ajax call is necessary.  
+// The file also contains related message dialogs(using bootbox) to be displayed as response to ajax requests
+// 20190826
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function to make ajax calls from the project.
+// RESULT:
+// If the result is successful, display success message dialog and redirect to "received url" in "json result from action"
+// If the result is NOT successful, display the message dialog. Enable the sender button after user close the message dialog.
+// PARAMS:
+// -senderButton is the button calls the function and used for enable and disable the button
+// -sendData is the data to be sent within the ajax request. It is the parameter of the action.
+// -callUrl is the url for the ajax post
+// -requestVerificationToken is needed to be able to reach controller action which uses ValidateAntiForgeryToken.
+function postAjaxAndDisplayResultDialog(senderButton, sendData, callUrl, requestVerificationToken) {
+
+    $.ajax({
+        url: callUrl,
+        type: "POST",
+        data: sendData,
+        dataType: "json",
+        headers: { 'RequestVerificationToken': requestVerificationToken },
+        beforeSend: function () {
+            //$(preloader).show();//display preloader.
+        },
+        success: function (data) {
+            //$(preloader).hide();//hide preloader.
+
+            if (data.state === 0) {
+                DisplaySuccessMessage(data.msg, data.redirectUrlInSuccess);// display result returned from method in controller (successful or failed)
+            } else {
+                DisplayFailureMessage(data.msg);// display result returned from method in controller (successful or failed)
+                $(senderButton).attr("disabled", false);//Enable sender button 
+            }
+        },
+        failure: function (response) {
+            $(preloader).hide();//hide preloader
+            DisplayFailureMessage(response);// display failure response of ajax call
+            $(senderButton).attr("disabled", false);//Enable sender button
+        }
+    });
+}
+
+
+/// Display given message in a dialog which has a green check circle in title.
+/// As an addition, if redirectUrl is not empty, it redirects to that page, 
+/// after user closed the dialog box
+function DisplaySuccessMessage(msg, redirectUrl) {
+    bootbox.dialog({
+        title: "<i class=\"fa fa-check-circle\" style=\"color: green;\"></i> Successful",
+        message: msg,
+        buttons: {
+            main: {
+                label: "OK",
+                className: "btn-primary",
+                callback: function () {
+                    if (redirectUrl === undefined || redirectUrl === null || redirectUrl === "") {
+                        return;
+                    } else {
+                        location.href = redirectUrl;//Go to redirectUrl after user closed displaying success message
+                    }
+                }
+            }
+        }
+    });
+}
+
+/// Display given message in a dialog which has a red exclamation mark in title
+function DisplayFailureMessage(msg) {
+    bootbox.dialog({
+        title: "<i class=\"fa fa-exclamation-circle\" style=\"color: red;\"></i> Failed",
+        message: msg,
+        buttons: {
+            main: {
+                label: "OK",
+                className: "btn-primary"
+            }
+        }
+    });
+}
